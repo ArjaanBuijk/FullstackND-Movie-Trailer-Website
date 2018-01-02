42899db6ff95d114c410b8362f0d7cf72a539a2f
Arjaan Buijk <arjaan.buijk@gmail.com>
2018-01-02 10:51:43 -0500

    Add Monty Python movies

---
+++ entertainment_center.py
@@ -0,0 +1,29 @@
+import media
+import fresh_tomatoes
+
+def list_movies():
+    """ Creates the list of movies to display in the web-page"""
+    movies = []
+    
+    movies.append(media.Movie(
+        "Monty Python's Life of Brian",
+        "Religious satire comedy",
+        "https://vignette1.wikia.nocookie.net/greatest-movies/images/3/3f/Monty_pythons_life_of_brian.jpg/revision/latest?cb=20170820100523",
+        "https://www.youtube.com/watch?v=HxIlg4m5fns"))
+    
+    movies.append(media.Movie(
+        "Monty Python and the Holy Grail",
+        "Slapstick comedy concerning the Arthurian legend",
+        "http://img.moviepostershop.com/monty-python-and-the-holy-grail-movie-poster-1975-1010465239.jpg",
+        "https://www.youtube.com/watch?v=hKNDml12Big"))    
+    
+    movies.append(media.Movie(
+        "Annie Hall",
+        "Romantic adventures of two neurotic New Yorkers",
+        "http://img.moviepostershop.com/annie-hall-movie-poster-1977-1010725380.jpg",
+        "https://www.youtube.com/watch?v=OqVgCfZX-yE"))
+        
+    return movies
+
+movies = list_movies()
+fresh_tomatoes.open_movies_page(movies)
