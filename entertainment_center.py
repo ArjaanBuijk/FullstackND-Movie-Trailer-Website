411aa9957494f65177ac3f58aee227d618072c7b
Arjaan Buijk <arjaan.buijk@gmail.com>
2018-01-03 07:45:15 -0500

    Fix code style and comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,22 +9,19 @@ def list_movies():
     movies.append(media.Movie(
         "Monty Python's Life of Brian",
         "Religious satire comedy",
-        "https://vignette1.wikia.nocookie.net/greatest-movies/images/3/3f/"
-        "Monty_pythons_life_of_brian.jpg/revision/latest?cb=20170820100523",
+        "https://vignette1.wikia.nocookie.net/greatest-movies/images/3/3f/Monty_pythons_life_of_brian.jpg/revision/latest?cb=20170820100523",  # NOQA
         "https://www.youtube.com/watch?v=HxIlg4m5fns"))
 
     movies.append(media.Movie(
         "Monty Python and the Holy Grail",
         "Slapstick comedy concerning the Arthurian legend",
-        "http://img.moviepostershop.com/"
-        "monty-python-and-the-holy-grail-movie-poster-1975-1010465239.jpg",
+        "http://img.moviepostershop.com/monty-python-and-the-holy-grail-movie-poster-1975-1010465239.jpg",  # NOQA
         "https://www.youtube.com/watch?v=hKNDml12Big"))
 
     movies.append(media.Movie(
         "Annie Hall",
         "Romantic adventures of two neurotic New Yorkers",
-        "http://img.moviepostershop.com/"
-        "annie-hall-movie-poster-1977-1010725380.jpg",
+        "http://img.moviepostershop.com/annie-hall-movie-poster-1977-1010725380.jpg",  # NOQA
         "https://www.youtube.com/watch?v=OqVgCfZX-yE"))
 
     return movies
--- media.py
+++ media.py
@@ -6,10 +6,19 @@ class Movie():
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """Stores the information related to a movie.
+
+        Args:
+            movie_title : the title of the movie
+            movie_storyline : the storyline of the movie
+            poster_image: url of a poster image
+            trailer_youtube: url of a youtube trailer
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """Plays the youtube trailer in the webbrowser."""
         webbrowser.open(self.trailer_youtube_url)
